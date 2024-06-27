ba7d62a83848ca332cee870bc82483bedbb11ff1
HebaZaki <h_borhan@hotmail.com>
2024-06-27 14:51:36 +0300

    javascript

---
+++ view/game.js
@@ -0,0 +1,98 @@
+document.querySelector(".buttons span").onclick = function () {
+    let UserName = prompt("Please Enter Your Name");
+
+    if (UserName == null || UserName == "") {
+        document.querySelector(".name span").innerHTML = "Unknown";
+    } else {
+        document.querySelector(".name span").innerHTML = UserName;
+    }
+
+    document.querySelector(".buttons").remove();
+};
+
+let duration = 2000;
+
+let gameBlocks = document.querySelector(".game-blocks");
+
+let cards = Array.from(gameBlocks.children);
+
+let cardsOrder = [...Array(cards.length).keys()];
+
+cardsShuffle(cardsOrder);
+
+// order css property to cards
+cards.forEach((card, index) => {
+    cardsOrder[index] = card.style.order;
+
+    // click event
+    card.addEventListener("click", function () {
+        cardsFlip(card);
+    });
+});
+
+
+// flipping cards
+function cardsFlip(clickedCard) {
+    // add class "is-clicked"
+    clickedCard.classList.add("is-clicked");
+
+    // all clicked cards
+    let clickedCards = cards.filter(clickedCard => clickedCard.classList.contains("is-clicked"));
+    // two flipped cards
+    if (clickedCards.length === 2) {
+        // unable to click another card
+        noClick();
+
+        // check if two cards are same
+        matchedCards(clickedCards[0], clickedCards[1]);
+    }
+}
+
+// stop clicking other cards
+function noClick() {
+    // add class no-click on game-blocks container
+    gameBlocks.classList.add("no-click");
+    // remove class no-click after 1 second
+    setTimeout(() => {
+        gameBlocks.classList.remove("no-click");
+    }, duration);
+}
+
+// matching cards
+function matchedCards(firstCard, secondCard) {
+    let flipsElement = document.querySelector(".flips span");
+
+    if (firstCard.dataset.potter === secondCard.dataset.potter) {
+        firstCard.classList.remove("is-clicked");
+        secondCard.classList.remove("is-clicked");
+
+        firstCard.classList.add("is-matched");
+        secondCard.classList.add("is-matched");
+    } else {
+        flipsElement.innerHTML = parseInt(flipsElement.innerHTML) + 1;
+        setTimeout(() => {
+            firstCard.classList.remove("is-clicked");
+            secondCard.classList.remove("is-clicked");
+        }, duration);
+    }
+}
+
+// shuffling cards
+function cardsShuffle(array) {
+    let currentIndex = array.length;
+    let randomIndex;
+    let temp;
+
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // save current number in temp
+        temp = array[currentIndex];
+        // current element = random
+        array[currentIndex] = array[randomIndex];
+        // random = get the number in temp
+        array[randomIndex] = temp;
+    };
+    return array;
+};
