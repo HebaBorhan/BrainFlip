a489b227c2c37fc3b7d49d61e64db62b637594cc
HebaBorhan <h_borhan@hotmail.com>
2024-07-02 20:11:51 +0300

    entry point

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from login import app as login_app
+from game import app as game_app
+from leaderboard import leaderboard_bp  # Assuming you use Blueprint here
+from dashboard import dashboard_bp  # Assuming you use Blueprint here
+from registration import app as registration_app
+
+app = Flask(__name__)
+
+# Register blueprints
+app.register_blueprint(login_app)
+app.register_blueprint(game_app)
+app.register_blueprint(leaderboard_bp)
+app.register_blueprint(dashboard_bp)
+app.register_blueprint(registration_app)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
